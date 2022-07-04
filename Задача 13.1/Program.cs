f5af06bb7aa786910b9d005d9b358793c2fa4c11
IrinaKaganovskaia <Kaganovskaia@icloud.com>
2022-07-04 14:58:00 +0300

    Задача через массив

---
+++ Задача 13.1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+//int[] array=[]
+System.Console.WriteLine("Введите число");
+string number=Console.ReadLine();
+ System.Console.WriteLine(number[2]);
+ if (number.Length<3) System.Console.WriteLine("Третьей цифры нет");
+
